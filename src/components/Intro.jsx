22d46ebeaae263ba5156b37161d8252245c0954a
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-02-27 20:47:04 +0530

    herosection

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,12 +1,13 @@
 function Intro() {
     return (
 
-        <div className="intro">
+        <div className="introsection">
+            <div className="intro">
             <h2 className="intro-idea">Uncover the fresh perspectives, ideas, and <br /> knowledge through the power of blogs.</h2>
-            <p className="intro-about">TheBlog Media is an open platform where readers find dynamic thinking, and where expert and undiscovered voices can share their writing on any topics. </p>
+            <p className="intro-about">TheBlog Media is an open platform where readers find dynamic thinking, and where expert and undiscovered <br /> voices can share their writing on any topics. </p>
             <button className="blog-read">Start Reading <i class="fa-solid fa-circle-arrow-right"></i></button>
             <button className="scroll-down">Scroll down <i class="fa-solid fa-arrow-down"></i></button>
-            <hr />
+        </div>
         </div>
     );
 }
