fe48d374c2fa719c171b5eb0afbfa1218eac88b5
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-02-27 21:23:03 +0530

    hero section update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -5,8 +5,8 @@ function Intro() {
             <div className="intro">
             <h2 className="intro-idea">Uncover the fresh perspectives, ideas, and <br /> knowledge through the power of blogs.</h2>
             <p className="intro-about">TheBlog Media is an open platform where readers find dynamic thinking, and where expert and undiscovered <br /> voices can share their writing on any topics. </p>
-            <button className="blog-read">Start Reading <i class="fa-solid fa-circle-arrow-right"></i></button>
-            <button className="scroll-down">Scroll down <i class="fa-solid fa-arrow-down"></i></button>
+            <button className="blog-read">Start Reading <i class="fa-solid fa-circle-arrow-right"></i></button>{/* 
+            <button className="scroll-down">Scroll down <i class="fa-solid fa-arrow-down"></i></button> */}
         </div>
         </div>
     );
