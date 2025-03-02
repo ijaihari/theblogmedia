750ef7307206d3a3b32232ea3e0af5a3a0e6b1ff
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-03-02 21:02:39 +0530

    logo update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,9 @@ function Header() {
         <div className="fixed">
             <header className="header">
                 <nav className="navbar">
-                    <h1 className="logo"><Link to="/">TheBlog Media</Link></h1>
-                    <ul className="navbar__tabs">
+                    <img className="logo" src='' alt="" />{/* 
+                    <h1 className="logo"><Link to="/">TheBlog Media</Link></h1> */}
+                    <ul className="navtabs">
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/blogs">Blogs</Link></li>
                         <li><Link to="/write">Write</Link></li>
