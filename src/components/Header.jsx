85147fa0e00ae2696ec070d9b00d3c79f64a713c
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-03-02 19:39:09 +0530

    update font style of logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,10 +13,10 @@ function Header() {
                         <li><Link to="/write">Write</Link></li>
                         <li><Link to="/community">Community</Link></li>
                     </ul>
-                    <section className="in-out">
+                    {/* <section className="in-out">
                         <button onClick={() => { navigate('/signin') }}>Sign in <i class="fa-solid fa-arrow-right-to-bracket"></i></button>
                         <button onClick={() => { navigate('/signout') }}>Sign up <i class="fa-solid fa-user-plus"></i></button>
-                    </section>
+                    </section> */}
                 </nav>
             </header>
         </div>
