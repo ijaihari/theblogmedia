b607979172acd9d896107fed628ec86d75c0b2ef
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-03-02 22:35:21 +0530

    update

--- src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import BlogReducer from './BlogSlice'
-
-export const store = configureStore({
-    reducer:{
-        blog:BlogReducer
-    }
-})
