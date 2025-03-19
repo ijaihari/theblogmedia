1c56d810373d35212abf50e07f74b21e85b2dff8
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-03-19 22:01:05 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,28 +1,34 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 function Header() {
     const navigate = useNavigate()
+    const [Open, setOpen] = useState(false);
+    function OpenClose() {
+        setOpen(!Open);
+    }
+
     return (
         <div className="fixed">
             <header className="header">
-                <nav className="navbar">                   
+                <nav className="navbar">
                     <h1 className="word-logo"><Link to="/theblogmedia">TheBlog Media</Link></h1>
-                    <button className="menu"><i class="fa-solid fa-bars"></i></button>
-                    <ul className="navtabs active-bar ">
+                    <button className="menu" onClick={() => { OpenClose() }}><i class="fa-solid fa-bars"></i></button>
+                    <ul className={Open ? "navtabs active-bar" : "navtabs close-bar"}>
                         <li><Link to="/theblogmedia">Home</Link></li>
                         <li><Link to="/blogs">Blogs</Link></li>
                         <li><Link to="/write">Write</Link></li>
                         <li><Link to="/community">Community</Link></li>
-                      </ul>
-                    
-                     <section className="in-out active-in-out">
+                    </ul>
+
+                    <section className={Open ? "in-out active-in-out" : "in-out close-bar"}>
                         <button onClick={() => { navigate('/signin') }}>Sign in <i class="fa-solid fa-arrow-right-to-bracket"></i></button>
                         <button onClick={() => { navigate('/signout') }}>Sign up <i class="fa-solid fa-user-plus"></i></button>
-                    </section> 
-                  
+                    </section>
+
                 </nav>
             </header>
-        </div>
+        </div >
     );
 }
 export default Header;
