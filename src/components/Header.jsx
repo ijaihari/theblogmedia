65d1af189a2af2faa88e00aea71efef5111b86ec
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-03-19 21:50:02 +0530

    complete media query

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,14 +8,14 @@ function Header() {
                 <nav className="navbar">                   
                     <h1 className="word-logo"><Link to="/theblogmedia">TheBlog Media</Link></h1>
                     <button className="menu"><i class="fa-solid fa-bars"></i></button>
-                    <ul className="navtabs">
+                    <ul className="navtabs active-bar ">
                         <li><Link to="/theblogmedia">Home</Link></li>
                         <li><Link to="/blogs">Blogs</Link></li>
                         <li><Link to="/write">Write</Link></li>
                         <li><Link to="/community">Community</Link></li>
                       </ul>
                     
-                     <section className="in-out">
+                     <section className="in-out active-in-out">
                         <button onClick={() => { navigate('/signin') }}>Sign in <i class="fa-solid fa-arrow-right-to-bracket"></i></button>
                         <button onClick={() => { navigate('/signout') }}>Sign up <i class="fa-solid fa-user-plus"></i></button>
                     </section> 
