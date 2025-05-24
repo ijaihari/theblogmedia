b5595a23666b2223842d8877874bdfa56aedc201
Jai Hari Nataraj <sjaiharinataraj@gmail.com>
2025-05-24 11:34:46 +0530

    footer update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ function Footer() {
     return (
         <div>
             <footer>
-                <p>&copy; TheBlog Media. All rights reserved | <a target="_blank" href="https://www.linkedin.com/in/ijaihari/">Created by Jai Hari Nataraj</a></p>
+                <p>&copy; TheBlog Media. All rights reserved | <a target="_blank" href="https://www.linkedin.com/in/ijaihari/">A project by Jai Hari Nataraj</a></p>
             </footer>
         </div>
     );
